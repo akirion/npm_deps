d0078183dcdf87e2798c99fa1e32b7475c741a4a
akirion <akiriondev@gmail.com>
2023-10-03 23:17:41 +0200

    Update npm_deps.ex
    Change get/1 to get/2 with optional opts argument
    Add json_library/0 to get configured JSON library

--- lib/npm_deps.ex
+++ lib/npm_deps.ex
@@ -7,16 +7,23 @@ defmodule NpmDeps do
 
   alias NpmDeps.Downloader
 
-  def get(deps) do
+  def get(deps, opts \\ []) do
     IO.puts("Downloading NPM packages...")
 
     deps
     |> Task.async_stream(
-      fn {namespace, version} -> Downloader.get(namespace, version) end,
+      fn {namespace, version} -> Downloader.get(namespace, version, opts) end,
       timeout: 60_000
     )
-    |> Enum.each(fn {:ok, {:ok, {namespace, version}}} ->
-      IO.puts("Downloaded #{namespace} #{version}")
+    |> Enum.each(fn {:ok, {:ok, {action, namespace, version}}} ->
+      case action do
+        :download -> IO.puts("Downloaded #{namespace} #{version}")
+        :keep -> IO.puts("Keep #{namespace} #{version}")
+      end
     end)
+  end
+
+  def json_library do
+    Application.get_env(:npm_deps, :json_library, Jason)
   end
 end
